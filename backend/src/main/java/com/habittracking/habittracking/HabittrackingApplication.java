f703cbc139680584dcc606a9687148151e3db3f7
Edu <serranotrenado.e@gmail.com>
2025-11-24 16:58:37 +0100

    initialized backend

---
+++ backend/src/main/java/com/habittracking/habittracking/HabittrackingApplication.java
@@ -0,0 +1,13 @@
+package com.habittracking.habittracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabittrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabittrackingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/habittracking/habittracking/HabittrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habittracking.habittracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabittrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
